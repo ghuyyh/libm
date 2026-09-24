ff27a7c67afd4f3d00e308f53cfff1fdd298a7ce
ghuyyh <95045672+ghuyyh@users.noreply.github.com>
2026-09-24 19:59:46 +0700

    fix Stage

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module com.pdm {
-    requires javafx.controls;
+    requires transitive javafx.controls;
+    requires transitive javafx.graphics;
     requires javafx.fxml;
 
     // Grants JavaFX access to your main application class
